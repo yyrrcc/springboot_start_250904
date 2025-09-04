d71ea2fb8dc6b1a26dd9500f7e84d5f7125c3c2a
yyrrcc <music_v@naver.com>
2025-09-04 15:31:51 +0900

    스프링부터 첫 시작

---
+++ src/main/java/com/mycompany/start/Start250904Application.java
@@ -0,0 +1,13 @@
+package com.mycompany.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Start250904Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Start250904Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/start/Start250904ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Start250904ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
